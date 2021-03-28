c40957bf77a869977d5f5f174a5f3a7920223b2c
KS <kyusang.dev@gmail.com>
2021-03-29 02:40:51 +0900

    first setting

---
+++ src/test/java/com/selfdevelopment/kyusang/KyusangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selfdevelopment.kyusang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyusangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
